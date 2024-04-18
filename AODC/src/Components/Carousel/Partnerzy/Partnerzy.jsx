e8a191eebc3a1d9aa2bfe9f079fb6eb33289829f
idzikowski20 <patryk.idzikowski@interia.pl>
2024-04-18 15:26:13 +0200

    changes

--- AODC/src/Components/Carousel/Partnerzy/Partnerzy.jsx
+++ AODC/src/Components/Carousel/Partnerzy/Partnerzy.jsx
@@ -12,46 +12,46 @@ const Partnerzy = () => {
     <div className="slider">
 	<div className="slide-track">
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+			<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 	</div>
 </div>
@@ -64,46 +64,46 @@ const Partnerzy = () => {
     <div className="slider">
 	<div className="slide-track">
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 		<div className="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+		<img src="assets/img/Carousel/cdcp.png" height="100" width="250" alt="" />
 		</div>
 	</div>
   </div>
