c54a667d71b3f0e607dc5498152f9ae137cb02d6
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-10 11:08:56 +0200

    .

--- src/Components/Hero/Hero2.jsx
+++ src/Components/Hero/Hero2.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './style.scss'
 import { withNamespaces } from 'react-i18next';
 import video from '../../../public/assets/background-video.mp4';
+import 'animate.css';
 
 function Hero2 ({ t }) {
   return (
@@ -10,8 +11,8 @@ function Hero2 ({ t }) {
   <div className='overlay-video'></div>
   <video className='video-background' src={video} autoPlay loop muted/>
 <div class='content'>
-    <h1 class='h1--scalingSize'>{t('Welcome to React')}</h1>
-    <h3 class='h3--scalingSize'>{t('for data center')}</h3>
+    <h1 class='h1--scalingSize animate__animated animate__backInDown'>{t('Welcome to React')}</h1>
+    <h3 class='h3--scalingSize animate__animated animate__backInUp'>{t('for data center')}</h3>
   </div>
 </div>
     </div>
