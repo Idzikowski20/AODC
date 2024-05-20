7b4e7114082a07438e6e5e79b30cdd22d43d8fd0
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-20 15:20:08 +0200

    .

--- src/Components/Modal/VideoModal.jsx
+++ src/Components/Modal/VideoModal.jsx
@@ -1,25 +1,26 @@
 import React, { useContext } from 'react';
 import { Modal } from 'react-bootstrap';
 import { JobContext } from '../../Context/JobContext';
+import { withNamespaces } from 'react-i18next';
 
-const VideoModal = () => {
-    const {handleCloseModal, showModal} = useContext(JobContext)
+
+
+function VideoModal({ t}) {
+    const {handleCloseModal, showModal} = useContext(JobContext)  
   return (
     <Modal show={showModal} onHide={handleCloseModal} size='lg' centered>
         <Modal.Body>
         <iframe
             width="100%"
-            height="315"
-            src="https://www.youtube.com/embed/8pcvWIfJvvQ"
+            height="400"
+            src={t('videomodal')}
             title="Video"
-            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
         ></iframe>
-        <button className='download-btn'>Pobierz prezentacje</button>
         </Modal.Body>
     </Modal>
   )
-}
+  }
 
-export default VideoModal
+export default withNamespaces()(VideoModal);
--- src/Components/Video Area/JobPageImage.jsx
+++ src/Components/Video Area/JobPageImage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const VideoArea3= () => {
+const JobPageImage = () => {
     return (
+      <>
       <div className="jm-video-area">
         <div className="container-fluid p-0">
           <div className="jm-video-wrap bg-default">
+            <div className='content'>
+              <h1>Lista dostępnych prac</h1> 
+            </div>
           </div>
         </div>
       </div>
+      </>
     );
   };
   
 
-export default VideoArea3
+export default JobPageImage
