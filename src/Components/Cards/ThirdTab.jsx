23a76db39da2d03677d4407e7c44e794c0c46766
Idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-21 23:09:32 +0200

    .

--- src/Components/Cards/ThirdTab.jsx
+++ src/Components/Cards/ThirdTab.jsx
@@ -4,7 +4,7 @@ import { withNamespaces } from 'react-i18next';
 function ThirdTab({ t }) {
   return (
     <div className="carrier-card-benefit">
-    <div>
+    <div className="what-we-do-title-container">
     <h1>{t('title2.budowa')}</h1>
       <p>{t('title2.budowa')}</p>
       <p>{t('title2.budowa3')}</p>
