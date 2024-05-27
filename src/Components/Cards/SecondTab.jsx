c93fcae5befca616407caedf0c9f1bfb8eb572d6
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-27 09:35:35 +0200

    .

--- src/Components/Cards/SecondTab.jsx
+++ src/Components/Cards/SecondTab.jsx
@@ -6,10 +6,10 @@ function SecondTab({ t }) {
     <div className="carrier-card-benefit">
     <div className="what-we-do-title-container">
     <h1>{t('title2.projekty')}</h1>
-      <p>{t('title2.projekty2')}</p>
-      <p>{t('title2.projekty3')}</p>
-      <p>{t('title2.projekty4')}</p>
-      <p>{t('title2.projekty5')}</p>
+      <p>• {t('title2.projekty2')}</p>
+      <p>• {t('title2.projekty3')}</p>
+      <p>• {t('title2.projekty4')}</p>
+      <p>• {t('title2.projekty5')}</p>
     </div>
     <div className="carrier-card-benefit-img">
     <img src="assets/img/blog/blog-quote.jpg"></img>
