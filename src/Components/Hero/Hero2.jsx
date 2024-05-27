21bb0ef05eabf4a310c0b469262d1ae418da68a7
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-27 12:16:07 +0200

    .

--- src/Components/Hero/Hero2.jsx
+++ src/Components/Hero/Hero2.jsx
@@ -10,9 +10,9 @@ function Hero2 ({ t }) {
         <div className='bluur'></div>
 <div className='hero-container'>
   <div className='bluur2'></div>
-<div class='content'>
-    <h1 class='h1--scalingSize animate__animated animate__backInDown'>{t('Welcome to React')}</h1>
-    <h3 class='h3--scalingSize animate__animated animate__backInUp'>{t('for data center')}</h3>
+<div className='content'>
+    <h1  className='h1--scalingSize animate__animated animate__backInDown'>{t('Welcome to React')}</h1>
+    <h3  className='h3--scalingSize animate__animated animate__backInUp'>{t('for data center')}</h3>
   </div>
   <div className='video-background-container'>
     <img className='video-background' src={video} autoPlay loop muted/>
