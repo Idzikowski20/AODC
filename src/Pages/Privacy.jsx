5752bb2ab97152545521c9e1060d2a71ff8d33e3
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-27 15:58:03 +0200

    .

--- src/Pages/Privacy.jsx
+++ src/Pages/Privacy.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+
 import PrivacyMain from '../Components/Privacy/PrivacyMain'
+import React from 'react'
+import Footer2 from '../Components/Footer/Footer2'
+import Header2 from '../Components/Header/Header2'
+import SidePanel from '../Components/Side Panel/SidePanel'
 
 const Privacy = () => {
   return (
     <>
+        <Header2 />
+    <SidePanel />
     <PrivacyMain />
+    <Footer2 />
     </>
   )
 }
