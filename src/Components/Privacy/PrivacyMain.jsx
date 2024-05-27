0b448d0113166cfed42fb2800a2d679f4e2983f7
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-27 15:22:11 +0200

    .

---
+++ src/Components/Privacy/PrivacyMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrivacyPageImage from '../Video Area/PrivacyPageImage'
+
+const PrivacyMain = () => {
+  return (
+    <>
+    <PrivacyPageImage />
+    </>
+  )
+}
+
+export default PrivacyMain
---
+++ src/Pages/Privacy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrivacyMain from '../Components/Privacy/PrivacyMain'
+
+const Privacy = () => {
+  return (
+    <>
+    <PrivacyMain />
+    </>
+  )
+}
+
+export default Privacy
