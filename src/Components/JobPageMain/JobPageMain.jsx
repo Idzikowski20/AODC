08be66939f83122274dfeb2a6f5a75f6e2c30b19
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-28 13:17:23 +0200

    .

--- src/Components/JobPageMain/JobPageMain.jsx
+++ src/Components/JobPageMain/JobPageMain.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import CarrierPageImage from '../Video Area/CarrierPageImage'
 
 const JobPageMain = () => {
@@ -6,9 +7,16 @@ const JobPageMain = () => {
     <>
     <CarrierPageImage />
     <section>
-      <div className='job-title'>
-      <h2>Oferty pracy</h2>
-      </div>
+    <div className='career-title'>
+    <div>
+    <p>Przed aplikacją na dane stanowisko prosimy o zapoznanie się z Polityką Prywatności.</p>
+    </div>
+    <div>
+      <Link to="/Polityka">
+      <button className="career-button">Polityka Prywatności</button>
+      </Link>
+    </div>
+    </div>
     <div className='job-container'>
     <iframe loading='lazy' className='job-cards' src="https://widgets.commoninja.com/iframe/c0f6ec33-7637-4a93-becc-210c8c6b6b9c" frameborder="0" scrolling="no"></iframe>
   </div>
