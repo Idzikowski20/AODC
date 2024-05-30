133adae3e10c354dce63fcdff52cf1c8fc94cee4
Idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-30 20:30:28 +0200

    .

--- src/Components/Hero/Hero2.jsx
+++ src/Components/Hero/Hero2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.scss'
 import { withNamespaces } from 'react-i18next';
-import video from '../../../public/assets/img/blog/blog-list-2.jpg'
+import video from '../../../public/assets/hero.mp4'
 import 'animate.css';
 
 function Hero2 ({ t }) {
@@ -15,7 +15,7 @@ function Hero2 ({ t }) {
     <h3  className='h3--scalingSize animate__animated animate__backInUp'>{t('for data center')}</h3>
   </div>
   <div className='video-background-container'>
-    <img className='video-background' src={video} autoPlay loop muted/>
+    <video className='video-background' src={video} autoPlay loop muted/>
   </div>
 </div>
     </div>
