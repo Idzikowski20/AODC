e57155da26d80e2a667d91c34a0fb32786fc6242
idzikowski20 <patryk.idzikowski@interia.pl>
2024-05-31 09:43:36 +0200

    .

--- src/Components/Error/ErrorArea.jsx
+++ src/Components/Error/ErrorArea.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { withNamespaces } from 'react-i18next';
 import { Link } from 'react-router-dom'
 
-const ErrorArea = () => {
+function ErrorArea({ t }) {
   return (
     <div className="jm-error-area pt-100 pb-100">
         <div className="container">
@@ -11,9 +12,9 @@ const ErrorArea = () => {
                         <img src="assets/img/error/error.png" alt=""/>
                     </div>
                     <div className="jm-error-content text-center">
-                        <h3 className="jm-error-title">Ups! Nie ma takiej strony.</h3>
-                        <p className="text mb-30">Sprawdź czy poprawnie wpisałeś adres</p>
-                        <Link to="/" className="jm-theme-btn">Strona główna</Link>
+                        <h3 className="jm-error-title">{t('title12')}</h3>
+                        <p className="text mb-30">{t('title12.1')}</p>
+                        <Link to="/" className="jm-theme-btn">{t('title12.2')}</Link>
                     </div>
                 </div>
             </div>
@@ -22,4 +23,4 @@ const ErrorArea = () => {
   )
 }
 
-export default ErrorArea
+export default withNamespaces()(ErrorArea);
