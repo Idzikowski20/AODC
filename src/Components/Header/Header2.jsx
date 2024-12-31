a78715197683c35be813a71cd39a46005bec4f41
Idzikowski20 <patryk.idzikowski@interia.pl>
2024-12-31 10:58:31 +0100

    .

--- src/Components/Header/Header2.jsx
+++ src/Components/Header/Header2.jsx
@@ -53,24 +53,28 @@ function Header2 ({ t }) {
                         </div>
                         </Trans>
                     </div>
-                    <div className="languageFlag">
-                                <button className="first" >
-                                <svg viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="languageIconTitle" stroke="#ffffff" strokeWidth="1" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title id="languageIconTitle">Language</title> <circle cx="12" cy="12" r="10"></circle> <path strokeLinecap="round" d="M12,22 C14.6666667,19.5757576 16,16.2424242 16,12 C16,7.75757576 14.6666667,4.42424242 12,2 C9.33333333,4.42424242 8,7.75757576 8,12 C8,16.2424242 9.33333333,19.5757576 12,22 Z"></path> <path strokeLinecap="round" d="M2.5 9L21.5 9M2.5 15L21.5 15"></path> </g></svg>
-                                </button>
-                                <button className="second" onClick={() => changeLanguage('en')} >
-                                <img src="https://cdn.countryflags.com/thumbs/united-kingdom/flag-400.png"/>
-                                </button>
-                                <button className="third" onClick={() => changeLanguage('pl')}>
-                                <img src="https://cdn.countryflags.com/thumbs/poland/flag-400.png"/>
-                                </button>
-                    </div>
-                        <div className="px100 menu-mobile jm-header-right jm-header-right-2 text-end  d-flex justify-content-center">
-                            <div className="jm-navbar-mobile-sign side-toggle d-lg-none d-inline-block" role='button' onClick={handleOpen}>
-                                <span className="dr-line-1"></span>
-                                <span className="dr-line-2"></span>
-                                <span className="dr-line-3"></span>
+
+                    <div className='mobile-menu-container'>
+                        <div className="languageFlag">
+                                    <button className="first" >
+                                    <svg viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="languageIconTitle" stroke="#ffffff" strokeWidth="1" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title id="languageIconTitle">Language</title> <circle cx="12" cy="12" r="10"></circle> <path strokeLinecap="round" d="M12,22 C14.6666667,19.5757576 16,16.2424242 16,12 C16,7.75757576 14.6666667,4.42424242 12,2 C9.33333333,4.42424242 8,7.75757576 8,12 C8,16.2424242 9.33333333,19.5757576 12,22 Z"></path> <path strokeLinecap="round" d="M2.5 9L21.5 9M2.5 15L21.5 15"></path> </g></svg>
+                                    </button>
+                                    <button className="second" onClick={() => changeLanguage('en')} >
+                                    <img src="https://cdn.countryflags.com/thumbs/united-kingdom/flag-400.png"/>
+                                    </button>
+                                    <button className="third" onClick={() => changeLanguage('pl')}>
+                                    <img src="https://cdn.countryflags.com/thumbs/poland/flag-400.png"/>
+                                    </button>
+                        </div>
+                            <div className="px100 menu-mobile jm-header-right jm-header-right-2 text-end  d-flex justify-content-center">
+                                <div className="jm-navbar-mobile-sign side-toggle d-lg-none d-inline-block" role='button' onClick={handleOpen}>
+                                    <span className="dr-line-1"></span>
+                                    <span className="dr-line-2"></span>
+                                    <span className="dr-line-3"></span>
                             </div>
                         </div>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/Components/Video Area/VideoArea.jsx
+++ src/Components/Video Area/VideoArea.jsx
@@ -2,18 +2,20 @@ import React, { useContext } from 'react';
 import { JobContext } from '../../Context/JobContext';
 import VideoModal from '../Modal/VideoModal';
 import { withNamespaces } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 function VideoArea({ t }) {
-  const {handleOpenModal} = useContext(JobContext)
+  // const {handleOpenModal} = useContext(JobContext)
     return (
       <div className="jm-video-area">
         <div className="container-fluid p-0">
           <div className="jm-video-wrap bg-default">
             <div className="jm-video-icon mb-30">
-              <a role='button' className="jm-play-btn" target='_blank' onClick={handleOpenModal}
-              >
+              <Link  target="_blank" to="https://youtu.be/lYkVhmQUq9I?si=PWvs5e4dkzUfUMvD">
+              <a role='button' className="jm-play-btn" target='_blank'>
                 <i className="fa-solid fa-play"></i>
-              </a>
+                </a>
+              </Link>
             </div>
             <div className="jm-video-text">
               <h2 className="title">{t('title7')}</h2>
