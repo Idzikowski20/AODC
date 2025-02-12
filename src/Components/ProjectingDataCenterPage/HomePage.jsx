74d5f10b1a46b81e237b65a487a4e4c86c98f99e
Idzikowski20 <patryk.idzikowski@interia.pl>
2025-02-12 12:30:01 +0100

    .

--- src/Components/ProjectingDataCenterPage/HomePage.jsx
+++ src/Components/ProjectingDataCenterPage/HomePage.jsx
@@ -22,25 +22,25 @@ function HomePageProjecting({ t }) {
      <div className='description-box-container'>
         <div className='description-box'>
         <br></br>
-          <span>{t('Header3.3.2')}</span>
+          <span className='border-radius20'>{t('Header3.3.2')}</span>
         </div>
         <div className='description-title'>
            <h4>{t('Header3.3.3')}</h4> 
         </div>
         <div className='description-box'>
-          <span>{t('Header3.3.4')}</span><br></br>
-          <span>{t('Header3.3.5')}</span><br></br>
+          <span className='border-radius20'>{t('Header3.3.4')}</span><br></br>
+          <span className='border-radius20'>{t('Header3.3.5')}</span><br></br>
           <span>{t('Header3.3.6')}</span>
           <span>{t('Header3.3.7')}</span>
           <span>{t('Header3.3.8')}</span><br></br>
-          <span>{t('Header3.3.9')}</span>
+          <span className='border-radius20'>{t('Header3.3.9')}</span>
         </div>
         <div className='description-title'>
         <h4>{t('Header3.3.10')}</h4> 
         </div>
         <div className='description-box'>
-          <span>{t('Header3.3.11')}</span><br></br>
-          <span>{t('Header3.3.12')}</span><br></br>
+          <span className='border-radius20'>{t('Header3.3.11')}</span><br></br>
+          <span className='border-radius20'>{t('Header3.3.12')}</span><br></br>
         </div>
       </div>
     </div>
