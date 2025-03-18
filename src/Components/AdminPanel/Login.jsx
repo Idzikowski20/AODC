9d3f37a33f2006132bde43ab7dbbe17d57268c73
Idzikowski20 <patryk.idzikowski@interia.pl>
2025-03-18 14:55:12 +0100

    .

--- src/Components/AdminPanel/Login.jsx
+++ src/Components/AdminPanel/Login.jsx
@@ -74,7 +74,7 @@ function Login() {
   };
 
   return (
-    <MDBContainer className="my-5 gradient-form">
+    <MDBContainer className="gradient-form">
       <div className="row login-container">
         <MDBCol col="6" className="mb-5">
           <div className="d-flex flex-column ms-5 me-5">
@@ -129,13 +129,8 @@ function Login() {
 
         <MDBCol col="6" className="mb-5">
           <div className="d-flex flex-column justify-content-center gradient-custom-2 h-100 mb-4">
-            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-              <h4 className="mb-4">We are more than just a company</h4>
-              <p className="small mb-0">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
+            <div className="login-image">
+            <img src="/assets/datacenter.png" alt="aodc data center company"/>
             </div>
           </div>
         </MDBCol>
